1a4ff916b944842543113179e5f89e9a7492f709
hakimlh <abdelhakim.lahlou@gmail.com>
2018-01-13 23:34:37 +0000

    Age format

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
+
     @yield('MyCssStyle')
 
 
--- resources/views/pages/Profile.blade.php
+++ resources/views/pages/Profile.blade.php
@@ -11,29 +11,45 @@
     box-shadow: 0 0 2px #ffe;
     border: 2px solide #fff;
   }
+  #mypanel{
+    color: black;
+    border-radius: 40px;
+
+  }
+  #mypanelfooter{
+    border-bottom-right-radius: 40px;
+    border-bottom-left-radius: 40px;
+  }
+
+  .panel h2 {
+    color : green;
+    font-style: oblique;
+  }
 </style>
 @endsection
 @section('content')
-<div class="row" style="margin-top:20px">
-  <div class="container">
-    <div class="col-md-6 col-md-offset-3">
-      <div class="panel panel-default">
+<header class="text-center" name="home">
+  <div class="intro-text">
+    <div class="col-md-4 col-md-offset-4" >
+      <div class="panel panel-default" id='mypanel'>
         <div class="panel-head text-center">
           <h3>Profile</h3>
           <img  class="img_profile" src="https://www.codetic.net/demo/templates/Privado/img/avatar.png" alt="">
         </div>
         <div class="panel-body text-center">
-          <h1>{{ $user->name }}</h1>
+          <h2>{{ $user->name }}</h2>
           <h3>{{ $user->username }}</h3>
-          <h4>{{ $user->dob }}</h4>
+          <h4>{{ $user->dob->format('Y-m-d') }} ({{ $user->dob->age }} years old)</h4>
           <h5>{{ $user->email }}</h5>
         </div>
-        <div class="panel-footer text-center">
-          <button type="button" class="bnt btn-default" name="button">Follow</button>
+        <div class="panel-footer text-center" id="mypanelfooter">
+          <button type="button" class="bnt btn-default btn-lg " name="button">Follow</button>
         </div>
-
       </div>
     </div>
+    <div class="clearfix"></div>
+    <!-- <a href="#about-section" class="btn btn-default btn-lg page-scroll">Learn More</a> -->
   </div>
-</div>
+</header>
+
 @endsection
