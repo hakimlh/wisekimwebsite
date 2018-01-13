ddf2d985259211c842afe08537461f91f532b369
hakimlh <abdelhakim.lahlou@gmail.com>
2018-01-13 23:01:39 +0000

    fix User model error

--- resources/views/pages/Profile.blade.php
+++ resources/views/pages/Profile.blade.php
@@ -6,6 +6,7 @@
     background-color: rgb(225,225,225);
   }
   .img_profile{
+    max-width: 150px;
     border-radius: 100%;
     box-shadow: 0 0 2px #ffe;
     border: 2px solide #fff;
@@ -14,22 +15,24 @@
 @endsection
 @section('content')
 <div class="row" style="margin-top:20px">
-  <div class="col-md-4 col-md-offset-4">
-    <div class="panel panel-default">
-      <div class="panel-head text-center">
-        <h3>Profile</h3>
-        <img  class="img_profile" src="https://www.codetic.net/demo/templates/Privado/img/avatar.png" alt="">
-      </div>
-      <div class="panel-body text-center">
-        <h1>{{ $user->name }}</h1>
-        <h3>{{ $user->username }}</h3>
-        <h4>{{ $user->dob }}</h4>
-        <h1>{{ $user->name }}</h1>
-      </div>
-      <div class="panel-footer text-center">
+  <div class="container">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="panel panel-default">
+        <div class="panel-head text-center">
+          <h3>Profile</h3>
+          <img  class="img_profile" src="https://www.codetic.net/demo/templates/Privado/img/avatar.png" alt="">
+        </div>
+        <div class="panel-body text-center">
+          <h1>{{ $user->name }}</h1>
+          <h3>{{ $user->username }}</h3>
+          <h4>{{ $user->dob }}</h4>
+          <h5>{{ $user->email }}</h5>
+        </div>
+        <div class="panel-footer text-center">
+          <button type="button" class="bnt btn-default" name="button">Follow</button>
+        </div>
 
       </div>
-
     </div>
   </div>
 </div>
