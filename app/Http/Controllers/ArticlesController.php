b430aa8e9fd2860ec87f1010c0bba01538adb0b6
hakimlh <abdelhakim.lahlou@gmail.com>
2018-01-14 12:09:50 +0000

    Eloquent vs Query Builder : Create/Show

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,13 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Article;
 use Auth;
+use DB;
 
 class ArticlesController extends Controller
 {
 
     public function index()
     {
-        //
+      // 1 : Eloquent
+
+        // $articles = Article::all();
+        // $articles = Article::whereTitle('Game of thrones')->get();
+
+      // 2 : Query Builder
+
+        // $articles = DB::table('articles')->get();
+         $articles = DB::table('articles')->whereTitle('Game of thrones')->get();
+
+
+        return $articles;
     }
     public function create()
     {
@@ -19,6 +31,8 @@ class ArticlesController extends Controller
     }
     public function store(Request $request)
     {
+        // 1 : Eloquent
+
         // $article = new Article;
         // $article->title = $request->title;
         // $article->content = $request->content;
@@ -37,6 +51,18 @@ class ArticlesController extends Controller
         //   'user_id' => Auth::user()->id
         // ]);
 
+
+        // 2 : Query Builder
+
+        // DB::table('articles')->insert($request->except('_token')); // all data except _token input
+
+        // or
+
+        // DB::table('articles')->insert([
+        //   'title' => $request->title,
+        //   'content' => $request->content,
+        //   'user_id' => Auth::user()->id
+        // ]);
     }
     public function show($id)
     {
